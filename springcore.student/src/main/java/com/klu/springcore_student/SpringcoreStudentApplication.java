f651e1aee6abc193b8d47e61694178ae0de98c7c
manikonda-anjali <anjalireddy02010@gmail.com>
2026-02-11 16:35:00 +0530

    First commit

---
+++ springcore.student/src/main/java/com/klu/springcore_student/SpringcoreStudentApplication.java
@@ -0,0 +1,13 @@
+package com.klu.springcore_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreStudentApplication.class, args);
+	}
+
+}
