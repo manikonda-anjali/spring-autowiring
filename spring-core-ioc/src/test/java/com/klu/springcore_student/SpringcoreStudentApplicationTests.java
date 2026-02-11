112747f45a5f7257f215adb75196fc752cbab53d
manikonda-anjali <anjalireddy02010@gmail.com>
2026-02-11 16:31:03 +0530

    First commit

---
+++ spring-core-ioc/src/test/java/com/klu/springcore_student/SpringcoreStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu.springcore_student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
